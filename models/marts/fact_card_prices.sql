1ac250bd0ae981fd1004b4e6176bb7b616d569c2
Ivan Campos <sayanegro9@gmail.com>
2025-05-17 17:02:15 +0000

    mas pruebas

--- models/marts/fact_card_prices.sql
+++ models/marts/fact_card_prices.sql
@@ -5,45 +5,28 @@
     incremental_strategy = 'append'
 ) }}
 
-WITH base AS (
+WITH retail_normal AS (
     SELECT
         card_id,
-        paper_prices
+        provider,
+        TRY_PARSE_JSON(retail_prices) AS retail_prices_json
     FROM {{ ref('stg_prices') }}
-    WHERE paper_prices IS NOT NULL
-),
-
-providers AS (
-    SELECT
-        card_id,
-        provider.key::string AS provider_name,
-        provider.value AS provider_data
-    FROM base,
-         LATERAL FLATTEN(input => paper_prices) provider
-),
-
-retail_normal AS (
-    SELECT
-        card_id,
-        provider_name,
-        TRY_PARSE_JSON(provider_data:retail:normal) AS retail_normal_prices
-    FROM providers
-    WHERE provider_data:retail:normal IS NOT NULL
+    WHERE retail_prices IS NOT NULL
 ),
 
 prices_flattened AS (
     SELECT
         card_id,
-        provider_name,
-        price_entry.key::string AS price_date_str,
-        price_entry.value::string AS price_value_str
+        provider,
+        price.key::string AS price_date_str,
+        price.value::string AS price_value_str
     FROM retail_normal,
-         LATERAL FLATTEN(input => retail_normal_prices) price_entry
+         LATERAL FLATTEN(input => retail_prices_json) price
 )
 
 SELECT
     card_id,
-    provider_name AS provider,
+    provider,
     TO_DATE(price_date_str) AS price_date,
     TRY_TO_DECIMAL(price_value_str, 10, 2) AS price
 FROM prices_flattened
