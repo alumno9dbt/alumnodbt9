949f7661b582ef06447f57d2e0e75c7364d71a9c
Ivan Campos <sayanegro9@gmail.com>
2025-05-17 17:57:26 +0000

    ...

--- models/staging/stg_identifiers.sql
+++ models/staging/stg_identifiers.sql
@@ -1,7 +1,23 @@
-{{ config(materialized='view') }}
+{{ config(materialized = 'view') }}
 
-SELECT
-    card_id,
-    card_id AS scryfall_id
-FROM {{ ref('stg_cards') }}
-WHERE card_id IS NOT NULL
+WITH printings AS (
+    SELECT raw AS set_object
+    FROM {{ source('bronze', 'raw_printings') }}
+),
+
+identifiers_flat AS (
+    SELECT
+        card.value:identifiers:scryfallId::string AS scryfall_id,
+        card.value:identifiers:mtgjsonV4Id::string AS mtgjsonv4_id
+    FROM printings,
+         LATERAL FLATTEN(input => set_object:"cards") AS card
+    WHERE card.value:identifiers:scryfallId IS NOT NULL
+      AND card.value:identifiers:mtgjsonV4Id IS NOT NULL
+)
+
+SELECT DISTINCT
+    mtgjsonv4_id,
+    scryfall_id AS card_id
+FROM identifiers_flat
+WHERE mtgjsonv4_id IS NOT NULL
+  AND card_id IS NOT NULL
--- models/staging/stg_prices.sql
+++ models/staging/stg_prices.sql
@@ -3,37 +3,29 @@
 WITH raw_prices AS (
     SELECT
         card.key::string AS mtgjsonv4_id,
-        card.value AS card_data
+        card.value AS price_data
     FROM {{ source('bronze', 'raw_prices') }},
-         LATERAL FLATTEN(input => raw) card
+         LATERAL FLATTEN(input => raw) AS card
 ),
 
-identifiers AS (
-    SELECT
-        card.value:identifiers:mtgjsonV4Id::string AS mtgjsonv4_id,
-        card.value:identifiers:scryfallId::string AS card_id
-    FROM {{ source('bronze', 'raw_printings') }},
-         LATERAL FLATTEN(input => raw:"cards") card
-    WHERE card.value:identifiers:mtgjsonV4Id IS NOT NULL
-      AND card.value:identifiers:scryfallId IS NOT NULL
-),
-
+-- Vincula el ID de precios (mtgjsonv4_id) con el ID de cartas (scryfallId)
 joined AS (
     SELECT
         i.card_id,
-        rp.card_data:"paper" AS paper_prices
+        rp.price_data:"paper" AS paper_prices
     FROM raw_prices rp
-    JOIN identifiers i ON rp.mtgjsonv4_id = i.mtgjsonv4_id
-    WHERE rp.card_data:"paper" IS NOT NULL
+    JOIN {{ ref('stg_identifiers') }} i
+      ON rp.mtgjsonv4_id = i.mtgjsonv4_id
+    WHERE rp.price_data:"paper" IS NOT NULL
 ),
 
-exploded AS (
+flattened AS (
     SELECT
         card_id,
         provider.key::string AS provider,
         provider.value AS provider_data
     FROM joined,
-         LATERAL FLATTEN(input => paper_prices) provider
+         LATERAL FLATTEN(input => paper_prices) AS provider
     WHERE provider.value:retail:normal IS NOT NULL
 )
 
@@ -41,4 +33,4 @@ SELECT
     card_id,
     provider,
     TRY_PARSE_JSON(provider_data:retail:normal) AS retail_prices
-FROM exploded
+FROM flattened
