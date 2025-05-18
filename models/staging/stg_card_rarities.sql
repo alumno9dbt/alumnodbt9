9f865015023a99d08fd84cbf41c3554e8e455717
Ivan Campos <sayanegro9@gmail.com>
2025-05-18 16:37:51 +0000

    borron y cuenta nueva

--- models/staging/stg_card_rarities.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT
-    card_id,
-    rarity
-FROM {{ ref('stg_cards') }}
-WHERE card_id IS NOT NULL
-  AND rarity IS NOT NULL
--- models/staging/stg_identifiers.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT
-    card_id,
-    scryfall_id,
-    mtgjsonv4_id
-FROM {{ ref('stg_cards') }}
-WHERE card_id IS NOT NULL
-  AND (scryfall_id IS NOT NULL OR mtgjsonv4_id IS NOT NULL)
