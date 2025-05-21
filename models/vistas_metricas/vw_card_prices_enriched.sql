a73be6d4603a26ba09cb9c3951337f5196f56129
Ivan Campos <sayanegro9@gmail.com>
2025-05-21 16:23:16 +0000

    seguimos

---
+++ models/vistas_metricas/vw_card_prices_enriched.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='table') }}
+
+with prices as (
+    select *
+    from {{ ref('fact_card_prices') }}
+),
+
+cards as (
+    select
+        card_id,
+        name,
+        set_name,
+        rarity,
+        main_types,
+        color_names
+    from {{ ref('dim_cards') }}
+)
+
+select
+    p.card_id,
+    c.name as card_name,
+    c.set_name,
+    c.rarity,
+    c.main_types as type_name,
+    c.color_names as color_name,
+    p.usd,
+    p.eur,
+    p.tix
+from prices p
+left join cards c on p.card_id = c.card_id
