5b724c7f05b262762c5939d4bc60049c694849e1
Ivan Campos <sayanegro9@gmail.com>
2025-05-22 10:29:58 +0000

    cambio chico

--- models/vistas_metricas/vw_card_prices_enriched.sql
+++ models/vistas_metricas/vw_card_prices_enriched.sql
@@ -12,7 +12,11 @@ cards as (
         set_name,
         rarity,
         main_types,
-        color_names
+        color_names,
+        case 
+            when lower(main_types) like '%legendary%' then true
+            else false
+        end as is_legendary
     from {{ ref('dim_cards') }}
 )
 
@@ -23,6 +27,7 @@ select
     c.rarity,
     c.main_types as type_name,
     c.color_names as color_name,
+    c.is_legendary,
     p.usd,
     p.eur,
     p.tix
